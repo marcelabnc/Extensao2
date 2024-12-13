8ba734d9d5c45c73fa1114a9cdb8af9ecba560f2
marcelabnc <141959355+marcelabnc@users.noreply.github.com>
2024-12-13 00:25:11 -0300

    Add files via upload

---
+++ globalization/translations.sql
@@ -0,0 +1,17 @@
+prompt --application/shared_components/globalization/translations
+begin
+--   Manifest
+--     TRANSLATIONS: 146762
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2024.05.31'
+,p_release=>'24.1.5'
+,p_default_workspace_id=>47256484443582396331
+,p_default_application_id=>146762
+,p_default_id_offset=>0
+,p_default_owner=>'WKSP_MARWORK'
+);
+null;
+wwv_flow_imp.component_end;
+end;
+/
