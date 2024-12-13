bd56ae4a81965356e3fc4cdf87d6094c2f2f8f2a
marcelabnc <141959355+marcelabnc@users.noreply.github.com>
2024-12-13 00:23:35 -0300

    Add files via upload

---
+++ deployment/definition.sql
@@ -0,0 +1,21 @@
+prompt --application/deployment/definition
+begin
+--   Manifest
+--     INSTALL: 146762
+--   Manifest End
+wwv_flow_imp.component_begin (
+ p_version_yyyy_mm_dd=>'2024.05.31'
+,p_release=>'24.1.5'
+,p_default_workspace_id=>47256484443582396331
+,p_default_application_id=>146762
+,p_default_id_offset=>0
+,p_default_owner=>'WKSP_MARWORK'
+);
+wwv_flow_imp_shared.create_install(
+ p_id=>wwv_flow_imp.id(85281726256508232157)
+,p_required_free_kb=>100
+,p_required_sys_privs=>'CREATE PROCEDURE:CREATE TABLE:CREATE TRIGGER:CREATE VIEW'
+);
+wwv_flow_imp.component_end;
+end;
+/
